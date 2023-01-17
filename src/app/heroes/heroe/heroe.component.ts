6ec6e58b47dd216237fd2030c7926456916514dd
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-01-17 11:54:56 -0600

    heroe component

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    //app, es para indicar que será parte de una aplicación personalizada
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html',
+})
+export class HeroeComponent{
+
+}
