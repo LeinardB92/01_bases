45b9cc312468eca64b8fd7a3a633602637682306
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-01-16 19:28:42 -0600

    se agregan estilios en style.css, se crea contador en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title: string = 'Contador App';
+  numero: number = 10;
+  base: number = 5;
+
+  operacion(valor: number){
+    this.numero += valor;
+  }
 }
