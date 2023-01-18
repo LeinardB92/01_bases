28ef94c5a2fe040a6a2b41f1168fcf5c6e09a2a6
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-01-17 18:39:47 -0600

    Borrar héroe

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -5,5 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './listado.component.html',
 })
 export class ListadoComponent {
- 
+  heroes: string[] = ['Spiderman', 'Chapulín colorado', 'Superman'];
+  heroeBorrado: string = '';
+
+  borrarHeroe(): void {
+    // _.shift() elimina el primer elemento de un array y lo retorna.
+    this.heroeBorrado = this.heroes.shift() || "";
+  }
+
+  
 }
