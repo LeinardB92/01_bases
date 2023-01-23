d59fed2d07819ca696eea20ce04ddefa10da328c
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-01-23 17:27:38 -0600

    Agregar personajes

--- src/app/dbz/mainpage/mainpage.component.ts
+++ src/app/dbz/mainpage/mainpage.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-interface personaje{
+interface Personaje{
   nombre: string,
   poder: number
 }
@@ -10,12 +10,31 @@ interface personaje{
   templateUrl: './mainpage.component.html',
 })
 export class MainpageComponent {
-  nuevo: personaje = {
-    nombre: 'Trunks',
-    poder: 14000
+  personajes: Personaje[] = [
+    {
+      nombre: "Goku",
+      poder: 15000
+    },
+    {
+      nombre: "Vegueta",
+      poder: 7500
+    }
+  ]
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
   }
 
   agregar(){
+    if(this.nuevo.nombre.trim().length === 0){
+      return
+    }
+    
     console.log(this.nuevo);
+
+    this.personajes.push(this.nuevo)
+    this.nuevo = {nombre:"", poder: 0}
   }
+
 }
